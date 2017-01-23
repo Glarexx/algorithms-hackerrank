999ac48fa210f6963b41eb8b38750856fa40cd84
Ievgenii Drozd <chap10@live.ru>
2017-01-23 10:55:04 -0500

    Security Functions

---
+++ security-security-function-1/src/Solution.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * @author Ievgenii Drozd
+ * @version 1/23/17
+ *          Security Functions
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        System.out.println(x % 11);
+
+    }
+}
